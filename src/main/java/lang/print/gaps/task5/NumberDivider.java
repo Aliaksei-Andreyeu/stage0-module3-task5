132f609c62b2b4caedb6d6b8b6f1997ed6a0af74
aliaksei-andreyeu <aliaksei.andreyeu@gmail.com>
2023-11-14 18:04:26 +0300

    Submit M3T5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double input = toBeDivided;
+        System.out.println(input/5);
     }
 }
